6e0a9fe9796d1bd233f6a2ef5de634e120853aeb
FrankieNay <89489233+FrankieNay@users.noreply.github.com>
2022-03-22 15:45:23 -0700

    Unity 1st Challenge almost done

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
-
+    private Vector3 offset = new Vector3(30, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,7 @@ public class FollowPlayerX : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
         transform.position = plane.transform.position + offset;
